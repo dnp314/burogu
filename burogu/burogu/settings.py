6206677cbc3bb4171cd8b24586f05b5069357aa8
Divyesh Parikh <dcparikh360@gmail.com>
2025-04-23 16:30:06 +0530

    Fix ALLOWED_HOSTS for deployment

--- burogu/burogu/settings.py
+++ burogu/burogu/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-miah3qk-r(%er!(uc56pg1cc=s)kgy(^w!1&_vs*3vqwc=88f5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['burogu.onrender.com']
 
 
 # Application definition
